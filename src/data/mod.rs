8a564c40e9ad79e852258e1229dc1cdac51796c5
shivanandvp <shivanandvp.oss@gmail.com>
2023-08-27 09:06:13 -0500

    feat: :sparkles: Complete the CLI

--- src/data/mod.rs
+++ src/data/mod.rs
@@ -1,14 +1,83 @@
+#[derive(
+    Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Serialize, Deserialize, Readable, Writable,
+)]
+pub enum HardwareKind {
+    #[serde(deserialize_with = "deserialize_struct_case_insensitive")]
+    #[serde(alias = "graphics", alias = "gpu")]
+    Graphics,
+
+    #[serde(deserialize_with = "deserialize_struct_case_insensitive")]
+    #[serde(alias = "ethernet", alias = "lan")]
+    Ethernet,
+
+    #[serde(deserialize_with = "deserialize_struct_case_insensitive")]
+    #[serde(alias = "wireless", alias = "wifi")]
+    Wireless,
+
+    #[serde(deserialize_with = "deserialize_struct_case_insensitive")]
+    #[serde(alias = "sound", alias = "audio")]
+    Audio,
+}
+
+impl FromStr for HardwareKind {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let user_string = String::from(s).to_lowercase();
+        match user_string
+            .to_ascii_lowercase()
+            .as_str()
+        {
+            "graphics" | "gpu" => Ok(HardwareKind::Graphics),
+            "ethernet" | "lan" => Ok(HardwareKind::Ethernet),
+            "wireless" | "wifi" => Ok(HardwareKind::Wireless),
+            "sound" | "audio" => Ok(HardwareKind::Audio),
+            _ => Err(Error::InvalidVariant {
+                value: s.into(),
+                enum_name: "HardwareKind".into(),
+                allowed_values: vec![
+                    "graphics", "gpu", "ethernet", "lan", "wireless", "wifi", "sound", "audio",
+                ]
+                .into_iter()
+                .map(|s| String::from(s))
+                .collect(),
+            }),
+        }
+    }
+}
+
+impl fmt::Display for HardwareKind {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            HardwareKind::Graphics => write!(f, "Graphics"),
+            HardwareKind::Ethernet => write!(f, "Ethernet"),
+            HardwareKind::Wireless => write!(f, "Wireless"),
+            HardwareKind::Audio => write!(f, "Audio"),
+        }
+    }
+}
+
 #[derive(Debug, Snafu)]
 #[non_exhaustive]
 pub enum Error {
     #[non_exhaustive]
-    #[snafu(display(""))]
-    DataDummy {},
+    #[snafu(
+        display("'{value}' is not recognized as a variant of '{enum_name}'. The allowed values are {allowed_values:?}")
+    )]
+    InvalidVariant {
+        value: String,
+        enum_name: String,
+        allowed_values: Vec<String>,
+    },
 }
 
 // region: IMPORTS
 
+use serde::{Deserialize, Serialize};
+use serde_aux::prelude::*;
 use snafu::Snafu;
+use speedy::{Readable, Writable};
+use std::{fmt, str::FromStr};
 
 // endregion: IMPORTS
 
--- src/ui/cli.rs
+++ src/ui/cli.rs
@@ -125,39 +125,212 @@ mod cli_template {
     pub struct CliTemplate {
         #[clap(flatten)]
         pub global_arguments: GlobalArguments,
+
+        #[clap(subcommand)]
+        pub command: Option<ActionCommand>,
+
+        #[clap(flatten)]
+        pub arguments: ListActionArguments,
     }
 
     #[derive(Debug, Args)]
-    #[clap(args_conflicts_with_subcommands = true)]
+    #[clap(args_conflicts_with_subcommands = true, next_display_order = usize::MAX - 100)]
     pub struct GlobalArguments {
         #[clap(
             long = "json",
             help = "Output in the JSON format for machine readability and scripting purposes.",
-            global = true
+            global = true,
+            display_order = usize::MAX - 4
         )]
         pub json_flag: bool,
 
         #[clap(
             long = "plain",
             help = "Output as plain text without extra information, for machine readability and scripting purposes.",
-            global = true
+            global = true,
+            display_order = usize::MAX - 3
         )]
         pub plain_flag: bool,
 
-        #[clap(long = "debug", help = "Output debug messages.", global = true)]
+        #[clap(
+            long = "debug",
+            help = "Output debug messages.",
+            global = true,
+            display_order = usize::MAX - 2
+        )]
         pub debug_flag: bool,
 
-        #[clap(long = "no-color", help = "Disable output coloring.", global = true)]
+        #[clap(
+            long = "no-color",
+            help = "Disable output coloring.",
+            global = true,
+            display_order = usize::MAX - 1
+        )]
         pub no_color_flag: bool,
 
         #[clap(flatten)]
         pub verbose: Verbosity<InfoLevel>,
     }
 
+    #[derive(Debug, Subcommand)]
+    #[clap(args_conflicts_with_subcommands = true)]
+    pub enum ActionCommand {
+        #[clap(name = "list", about = "List installed drivers.", display_order = 1)]
+        List(ListActionArguments),
+
+        #[clap(
+            name = "search",
+            about = "Search for available drivers.",
+            display_order = 2
+        )]
+        Search(SearchActionArguments),
+
+        #[clap(name = "install", about = "Install Drivers.", display_order = 3)]
+        Install(InstallActionArguments),
+
+        #[clap(
+            name = "generate-database",
+            aliases = ["generate-db", "gen-db", "gendb"],
+            about = "Generate database from input file.", 
+            display_order = 4
+        )]
+        GenerateDatabase(GenerateDatabaseActionArguments),
+    }
+
+    #[derive(Debug, Args)]
+    pub struct ListActionArguments {
+        #[clap(
+            value_enum,
+            help = "The hardware to list installed drivers for.",
+            display_order = 1
+        )]
+        pub hardware: Option<HardwareKind>,
+
+        #[clap(
+            long = "tags",
+            alias = "tag",
+            short = 't',
+            help = "Tag(s) to filter drivers.",
+            display_order = 2
+        )]
+        pub tags: Vec<String>,
+
+        #[clap(
+            long = "id",
+            short = 'i',
+            alias = "driver-id",
+            help = "ID to select a driver to look at.",
+            display_order = 3
+        )]
+        pub driver_id: Option<String>,
+
+        #[clap(
+            long = "database",
+            alias = "db",
+            help = "Path to the database file to use for recognizing drivers.",
+            display_order = 4
+        )]
+        pub database_file: Option<PathBuf>,
+    }
+
+    #[derive(Debug, Args)]
+    pub struct SearchActionArguments {
+        #[clap(
+            value_enum,
+            help = "The hardware to search drivers for.",
+            display_order = 1
+        )]
+        pub hardware: Option<HardwareKind>,
+
+        #[clap(
+            long = "tags",
+            alias = "tag",
+            short = 't',
+            help = "Tag(s) to filter drivers.",
+            display_order = 2
+        )]
+        pub tags: Vec<String>,
+
+        #[clap(
+            long = "id",
+            short = 'i',
+            alias = "driver-id",
+            help = "ID to select a driver to look for.",
+            display_order = 3
+        )]
+        pub driver_id: Option<String>,
+
+        #[clap(
+            long = "database",
+            alias = "db",
+            help = "Path to the database file to use for searching drivers.",
+            display_order = 4
+        )]
+        pub database_file: Option<PathBuf>,
+    }
+
+    #[derive(Debug, Args)]
+    pub struct InstallActionArguments {
+        #[clap(
+            value_enum,
+            help = "The hardware to list installed drivers for.",
+            display_order = 1
+        )]
+        pub hardware: Option<HardwareKind>,
+
+        #[clap(
+            long = "enable-aur",
+            alias = "aur",
+            help = "Enable installing from the Arch User Repository (AUR).",
+            display_order = 2
+        )]
+        pub enable_aur: bool,
+
+        #[clap(
+            long = "tags",
+            alias = "tag",
+            short = 't',
+            help = "Tag(s) to filter drivers.",
+            display_order = 3
+        )]
+        pub tags: Vec<String>,
+
+        #[clap(
+            long = "id",
+            short = 'i',
+            alias = "driver-id",
+            help = "ID to select a driver to look for.",
+            display_order = 4
+        )]
+        pub driver_id: Option<String>,
+
+        #[clap(
+            long = "database",
+            alias = "db",
+            help = "Path to the database file to use for recognizing drivers.",
+            display_order = 5
+        )]
+        pub database_file: Option<PathBuf>,
+    }
+
+    #[derive(Debug, Args)]
+    pub struct GenerateDatabaseActionArguments {
+        #[clap(
+            help = "Path to the human-readable input file (Only the YAML format is currently supported).",
+            display_order = 1
+        )]
+        pub input_file: PathBuf,
+
+        #[clap(help = "Path to the database file to generate.", display_order = 2)]
+        pub database_file: Option<PathBuf>,
+    }
+
     // region: IMPORTS
 
+    use crate::data::HardwareKind;
     use clap::{Args, Parser, Subcommand};
     use clap_verbosity_flag::{InfoLevel, Verbosity};
+    use std::path::PathBuf;
 
     // endregion: IMPORTS
 }
